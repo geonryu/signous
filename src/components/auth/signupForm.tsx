5e64d2a5af01c6f63d47b58a076464b4a08b7686
geonryu <60060831+geonryu@users.noreply.github.com>
2024-01-27 23:47:15 +0900

    test

--- src/components/auth/signupForm.tsx
+++ src/components/auth/signupForm.tsx
@@ -12,20 +12,20 @@ const Form = styled.form`
 
 const Scrollbar = styled.div`
     background-color: #dee2e6; border-radius: 100px; overflow: hidden; position: unset; height: 4px;
-    & span{display: block; height: 100%; background-color: #ff4600; transition: width 0.1s;}
+    & span{display: block; height: 100%; background-color: #ff4600; transition: width 0.3s;}
 `;
 
 
 export default function SignupForm() {
     const [step, setStep] = useState(1);
-    const signUpSteps = 5;
+    const signUpSteps = 4;
     
     return (
         <Section>
             <Wrap className="mx-auto col-md-8 col-lg-6">
                 <h3 className="mb-4 fw-bold text-center"><span className="fs-6 d-block text-primary">SIGNOUS</span> 회원가입</h3>
                 <Form action="">
-                    <div className="d-flex flex-wrap">
+                    <div className="d-flex flex-wrap fs-5">
                         <Scrollbar className={`w-100`}><span style={{ width: `${step / signUpSteps * 100}%` }} /></Scrollbar>
                         {/* <div className={`${step > 1 ? "text-gray-200" : ""} mt-1 me-1`}><button type="button" className="prevProgress"><span className="d-block fs-5 material-symbols-outlined">arrow_back</span></button></div> */}
                         <div className="fw-bold pt-3 pb-5 me-2">Step {step}.</div>
@@ -33,9 +33,7 @@ export default function SignupForm() {
                             {step === 1 ? <div>사이너스 서비스 이용약관에<br/>동의해주세요.</div> : null}
                             {step === 2 ? <div>로그인에 사용할<br/>아이디를 입력해주세요.</div> : null}
                             {step === 3 ? <div>로그인에 사용할<br/>비밀번호를 입력해주세요.</div> : null}
-                            {step === 4 ? <div>본인인증을 진행해주세요.
-안전한 거래를 위해 딱 한번 본인인증을 진행해요.</div> : null}
-                            {step === 5 ? <div>가입완료</div> : null}
+                            {step === 4 ? <div>본인인증을 진행해주세요.<div className="d-flex align-items-center fs-6"><span className="d-block material-symbols-rounded text-primary fs-5">verified_user</span>안전한 거래를 위해 딱 한번 본인인증을 진행해요.</div></div> : null}
                         </div>
                     </div>
                     <Swiper 
@@ -45,15 +43,12 @@ export default function SignupForm() {
                         onActiveIndexChange={(e)=>setStep(e.realIndex + 1)}
                         // allowTouchMove = {false}
                     >
-                        <SwiperSlide>Slide 1</SwiperSlide>
+                        <SwiperSlide>
+                            <input type="text" className="border w-100"/>
+                        </SwiperSlide>
                         <SwiperSlide>Slide 2</SwiperSlide>
                         <SwiperSlide>Slide 3</SwiperSlide>
                         <SwiperSlide>Slide 4</SwiperSlide>
-                        <SwiperSlide>Slide 5</SwiperSlide>
-                        <SwiperSlide>Slide 6</SwiperSlide>
-                        <SwiperSlide>Slide 7</SwiperSlide>
-                        <SwiperSlide>Slide 8</SwiperSlide>
-                        <SwiperSlide>Slide 9</SwiperSlide>
                     </Swiper>
                 </Form>
             </Wrap>
